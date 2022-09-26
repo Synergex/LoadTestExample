7a2b4e1e0424f6929117295d1c302a192ee4f112
Jeff Greene <hippiehunterenator@gmail.com>
2022-09-26 14:40:23 -0700

    hack for linux testing

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("press enter to initialize");
+Console.ReadLine();
+
+var threads = new List<Thread>();
+
+for (int i = 0; i < 5; i++)
+    threads.Add(new Thread(bob.billy.getThreadStart(i == 0)));
+
+
+foreach (var thrd in threads)
+{
+    thrd.IsBackground = false;
+    thrd.Start();
+    Thread.Sleep(1000);
+}
+
+
+Console.WriteLine("started");
+Thread.Sleep(50000);
+
+foreach (var thrd in threads)
+    thrd.Join();
+
+Console.WriteLine("joined");
+Console.ReadLine();
